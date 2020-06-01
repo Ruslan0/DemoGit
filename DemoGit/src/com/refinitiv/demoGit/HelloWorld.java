e440974c7589f2ddc228a05794935f5caa847a1f
Ruslan.Paluektau <Ruslan.Paluektau@refinitiv.com>
2020-06-01 21:11:50 +0300

    Added demogit.properties

--- DemoGit/src/com/refinitiv/demoGit/HelloWorld.java
+++ DemoGit/src/com/refinitiv/demoGit/HelloWorld.java
@@ -1,33 +1,36 @@
 package com.refinitiv.demoGit;
 
-//Import required java libraries
 import java.io.*;
+import java.util.Properties;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-//Extend HttpServlet class
 public class HelloWorld extends HttpServlet {
 
 private static final long serialVersionUID = -2473256499086594119L;
 private String message;
 
 public void init() throws ServletException {
-   // Do required initialization
-   message = "Hello World";
+   message = "Hello";
+}
+
+public String getName() throws FileNotFoundException, IOException {
+   Properties props = new Properties();
+   props.load(getServletContext().getResourceAsStream("/resources/demogit.properties"));
+   return (props.getProperty("name"));	
 }
 
 public void doGet(HttpServletRequest request, HttpServletResponse response)
    throws ServletException, IOException {
+
    
-   // Set response content type
    response.setContentType("text/html");
 
-   // Actual logic goes here.
    PrintWriter out = response.getWriter();
-   out.println("<h1>" + message + "</h1>");
+   out.println("<h1>" + message + getName()+"</h1>");
 }
 
 public void destroy() {
-   // do nothing.
 }
 }
